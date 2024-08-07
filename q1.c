08df5832a53db7d4ac92bf144924d58b4df51831
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-07 14:49:01 +0530

    Question_01

---
+++ q1.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/*
+Kavindu Sachinthe
+ECS
+galkotu-ec22053@stu.kln.ac.lk
+*/
+
+int main(){
+    int age; // c
+//Create an integer variable called as age.
+    age = 21;
+//Assign my age to the age integer variable.
+    printf("%d",age);
+//Print the value of the age variable.
+    return 0;
+
+
+}
