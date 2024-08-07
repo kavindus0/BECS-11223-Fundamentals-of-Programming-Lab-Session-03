db07fbd1d865f8b99bade50d8e4341768ad04cdc
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-07 14:49:53 +0530

    Question_02

---
+++ q2.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+/*
+Kavindu Sachinthe
+ECS
+galkotu-ec22053@stu.kln.ac.lk
+*/
+//•
+
+int main() {
+    int birth_year; // birth year
+    float z_score; // z scre variable
+    char physicsGrade; // phy grade
+//Assign the values to each variable you have declared in the previous step.
+    birth_year = 2003;
+    z_score = 1.1898;
+    physicsGrade = 'C';
+//Print the values of each variable in separate line.
+    printf("%d\n%.4f\n%c",birth_year,z_score,physicsGrade);
+
+    return 0;
+
+}
